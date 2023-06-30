cad04c8ff4afb1aa054e8b91710ec4a2d63f5cac
stavbers <reddevweb@gmail.com>
2023-06-30 22:02:05 +0300

    add new component

---
+++ src/components/FlipUnitContainer.jsx
@@ -0,0 +1,33 @@
+import {AnimatedCard} from './AnimatedCard'
+import {StaticCard} from './StaticCard'
+const FlipUnitContainer = ({ digit, shuffle, unit }) => {
+  let currentDigit = digit;
+  let previousDigit = digit - 1;
+  if (unit !== "hours") {
+    previousDigit = previousDigit === -1 ? 59 : previousDigit;
+  } else {
+    previousDigit = previousDigit === -1 ? 23 : previousDigit;
+  }
+  if (currentDigit < 10) {
+    currentDigit = `0${currentDigit}`;
+  }
+  if (previousDigit < 10) {
+    previousDigit = `0${previousDigit}`;
+  }
+
+  const digit1 = shuffle ? previousDigit : currentDigit;
+  const digit2 = !shuffle ? previousDigit : currentDigit;
+  const animation1 = shuffle ? "fold" : "unfold";
+  const animation2 = !shuffle ? "fold" : "unfold";
+
+  return (
+    <div className={"flipUnitContainer"}>
+      <StaticCard position={"upperCard"} digit={currentDigit} />
+      <StaticCard position={"lowerCard"} digit={previousDigit} />
+      <AnimatedCard digit={digit1} animation={animation1} />
+      <AnimatedCard digit={digit2} animation={animation2} />
+    </div>
+  );
+};
+
+export {FlipUnitContainer}
